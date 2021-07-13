d08da2ff33517c61e5c780e8f7d1727c9bd9ade1
Roland Bergauer <wi20m051@technikum-wien.at>
2021-07-13 22:33:59 +0200

    rechanged water-settings

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from menu import MenuItem#, Menu
 # from money_machine import MoneyMachine
 
 
-espresso = MenuItem(name="Espresso0815", cost=3, milk=200)
-espresso.ingredients = {"water": 100, "coffee": 25}
+espresso = MenuItem(name="Espresso0815", cost=3, milk=200, water=100, coffee=25)
+# espresso.ingredients = {"water": 100, "coffee": 25}
 
 
 print(espresso.name)
