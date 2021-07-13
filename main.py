8e699f9d71112d0f5cbc7999296b0783054e504c
Roland Bergauer <wi20m051@technikum-wien.at>
2021-07-13 22:33:16 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from menu import MenuItem#, Menu
+# from coffee_maker import CoffeeMaker
+# from money_machine import MoneyMachine
+
+
+espresso = MenuItem(name="Espresso0815", cost=3, milk=200)
+espresso.ingredients = {"water": 100, "coffee": 25}
+
+
+print(espresso.name)
+
