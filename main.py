61c994f6fd3382695806de51ccda6555f9cd7e65
Roland Bergauer <wi20m051@technikum-wien.at>
2021-07-14 23:15:46 +0200

    added for loop

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from menu import MenuItem#, Menu
 # from money_machine import MoneyMachine
 
 
-espresso = MenuItem(name="Espresso0815", cost=3, milk=200, water=100, coffee=25)
+espresso = MenuItem(name="Espresso", cost=3, milk=200, water=100, coffee=25)
+latte = MenuItem(name="Latte", cost=3, milk=200, water=100, coffee=25)
+cappuccino = MenuItem(name="Cappuccino", cost=3, milk=200, water=100, coffee=25)
 # espresso.ingredients = {"water": 100, "coffee": 25}
 
 
 print(espresso.name)
+for n in espresso.ingredients:
+    print(f"{n} = {espresso.ingredients[n]}")
 
